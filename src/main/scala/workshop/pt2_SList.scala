97f3626c91ced169a53e0f930df06b3382d2a20f
mtu <michal.tumilowicz01@gmail.com>
2024-03-10 00:26:19 +0100

    pt2_SList

---
+++ src/main/scala/workshop/pt2_SList.scala
@@ -0,0 +1,25 @@
+package workshop
+
+import scala.compiletime.ops.int.+
+
+object pt2_SList {
+  enum SList[N <: Int] {
+    self =>
+    case SNil extends SList[0]
+    case SCons[M <: Int](head: Int, tail: SList[M]) extends SList[M + 1]
+
+    def foldRight[B[_ <: Int]](z: B[0])(op: [M <: Int] => (Int, B[M]) => B[M + 1]): B[N] =
+      this match {
+        case SNil => z
+        case SCons(head, tail) => op(head, tail.foldRight(z)(op))
+      }
+
+    def ::(elem: Int): SList[N + 1] =
+      val newTail: SList[1] = SCons(elem, SNil)
+      foldRight[[X <: Int] =>> SList[X + 1]](newTail)([M <: Int] => SCons(_, _))
+
+    def reverse: SList[N] =
+      foldRight[[X <: Int] =>> SList[X]](SNil)([M <: Int] => (elem, acc) => acc.::(elem))
+  }
+
+}
---
+++ src/test/scala/workshop/pt2_SListSpec.scala
@@ -0,0 +1,35 @@
+package workshop
+
+import org.scalatest.Assertions.{assertCompiles, assertDoesNotCompile}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import workshop.pt2_SList.*
+import workshop.pt2_SList.SList.*
+
+class pt2_SListSpec extends AnyFlatSpec with Matchers {
+
+  it should "should not compile if size is wrong" in {
+    assertDoesNotCompile("val l1: SList[0] = 1 :: 2 :: 3 :: SNil")
+    assertDoesNotCompile("val l1: SList[1] = 1 :: 2 :: 3 :: SNil")
+    assertDoesNotCompile("val l1: SList[2] = 1 :: 2 :: 3 :: SNil")
+    assertDoesNotCompile("val l1: SList[4] = 1 :: 2 :: 3 :: SNil")
+    assertDoesNotCompile("val l1: SList[5] = 1 :: 2 :: 3 :: SNil")
+  }
+
+  it should "should compile if size is correct" in {
+    assertCompiles("val l1: SList[0] = SNil")
+    assertCompiles("val l1: SList[1] = 1 :: SNil")
+    assertCompiles("val l1: SList[2] = 1 :: 2 :: SNil")
+    assertCompiles("val l1: SList[3] = 1 :: 2 :: 3 :: SNil")
+    assertCompiles("val l1: SList[4] = 1 :: 2 :: 3 :: 4 :: SNil")
+  }
+
+  it should "should correctly reverse" in {
+    (1 :: 2 :: 3 :: 4 :: SNil).reverse shouldBe (4 :: 3 :: 2 :: 1 :: SNil)
+    (1 :: 2 :: 3 :: SNil).reverse shouldBe (3 :: 2 :: 1 :: SNil)
+    (1 :: 2 :: SNil).reverse shouldBe (2 :: 1 :: SNil)
+    (1 :: SNil).reverse shouldBe (1 :: SNil)
+    (SNil).reverse shouldBe (SNil)
+  }
+
+}
