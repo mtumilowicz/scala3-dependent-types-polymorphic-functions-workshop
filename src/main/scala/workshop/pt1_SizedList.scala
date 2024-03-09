cb1512fff852d986892e4efc357a2320e158e3c4
mtu <michal.tumilowicz01@gmail.com>
2024-03-10 00:03:51 +0100

    pt1_SizedList

--- src/main/scala/workshop/pt1_SizedList.scala
+++ src/main/scala/workshop/pt1_SizedList.scala
@@ -7,15 +7,17 @@ object pt1_SizedList {
   final class SizedList[N <: Int & Singleton, A] private(private val values: List[A]) {
     def ++[M <: Int  & Singleton](that: SizedList[M, A]): SizedList[(N + M) & Singleton, A] =
       new SizedList(values ++ that.values)
-
+    def ::(x: A): SizedList[(N + 1) & Singleton, A] = new SizedList(x +: values)
     def head(using (N > 0) =:= true): A = values.head
+    def tail(using (N > 0) =:= true): SizedList[(N - 1) & Singleton, A] = new SizedList(values.tail)
+    def toList: List[A] = values
   }
 
   object SizedList {
     def empty[A]: SizedList[0, A] = new SizedList(List.empty[A])
-
     def apply[A](a1: A): SizedList[1, A] = new SizedList(List(a1))
     def apply[A](a1: A, a2: A): SizedList[2, A] = new SizedList(List(a1, a2))
+
   }
 
   val v0: SizedList[0, Int] = SizedList.empty
@@ -23,6 +25,7 @@ object pt1_SizedList {
   val v2: SizedList[2, Int] = SizedList(2, 3)
   val v3: SizedList[3, Int] = v1 ++ v2
   val v4: SizedList[4, Int] = v1 ++ v3
+  val v5: SizedList[5, Int] = 1 :: 2 :: 3 :: 4 :: 5 :: SizedList.empty
   val hV4 = v1.head
 
 }
---
+++ src/test/scala/workshop/pt1_SizedListSpec.scala
@@ -0,0 +1,59 @@
+package workshop
+
+import org.scalatest.Assertions.{assertCompiles, assertDoesNotCompile}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import workshop.pt1_SizedList.*
+
+class pt1_SizedListSpec extends AnyFlatSpec with Matchers {
+
+  it should "should not compile if size is wrong" in {
+    assertDoesNotCompile("val l1: SizedList[0, 0] = 1 :: 2 :: 3 :: SizedList.empty")
+    assertDoesNotCompile("val l1: SizedList[0, 1] = 1 :: 2 :: 3 :: SizedList.empty")
+    assertDoesNotCompile("val l1: SizedList[0, 2] = 1 :: 2 :: 3 :: SizedList.empty")
+    assertDoesNotCompile("val l1: SizedList[0, 4] = 1 :: 2 :: 3 :: SizedList.empty")
+    assertDoesNotCompile("val l1: SizedList[0, 5] = 1 :: 2 :: 3 :: SizedList.empty")
+  }
+
+  it should "should compile if size is correct" in {
+    assertCompiles("val l1: SizedList[0, Int] = SizedList.empty[Int]")
+    assertCompiles("val l1: SizedList[1, Int] = 1 :: SizedList.empty")
+    assertCompiles("val l1: SizedList[2, Int] = 1 :: 2 :: SizedList.empty")
+    assertCompiles("val l1: SizedList[3, Int] = 1 :: 2 :: 3 :: SizedList.empty")
+    assertCompiles("val l1: SizedList[4, Int] = 1 :: 2 :: 3 :: 4 :: SizedList.empty")
+  }
+
+  it should "should not compile if calling head on empty" in {
+    assertDoesNotCompile("val l1 = SizedList.empty; l1.head")
+  }
+
+  it should "should compile if calling head on non empty" in {
+    assertCompiles("val l1 = 1 :: SizedList.empty; l1.head")
+  }
+
+  it should "should not compile if calling head on empty after operations" in {
+    assertDoesNotCompile("val l1 = 1 :: 2 :: SizedList.empty; l1.tail.tail.head")
+  }
+
+  it should "should not compile if calling tail on empty after operations" in {
+    assertDoesNotCompile("val l1 = 1 :: 2 :: SizedList.empty; l1.tail.tail.tail")
+  }
+
+  it should "return correct head if calling on non empty" in {
+    val l1 = 1 :: 2 :: 3 :: 4 :: SizedList.empty
+    assert(l1.head == 1)
+  }
+
+  it should "return correct tail if calling on non empty" in {
+    val l1 = 1 :: 2 :: 3 :: 4 :: SizedList.empty
+    val expectedTail = 2 :: 3 :: 4 :: SizedList.empty
+    assert(l1.tail.toList == expectedTail.toList)
+  }
+
+  it should "return empty if called head sufficient number of times" in {
+    val l1 = 1 :: 2 :: 3 :: 4 :: SizedList.empty
+    val result: SizedList[0, Int] = l1.tail.tail.tail.tail
+    assert(result.toList == SizedList.empty.toList)
+  }
+
+}
