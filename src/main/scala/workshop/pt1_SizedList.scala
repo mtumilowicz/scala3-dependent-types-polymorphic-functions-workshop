f23b0538e7bc47e469e06c99e180d0bbf03b837e
mtu <michal.tumilowicz01@gmail.com>
2024-03-09 23:34:33 +0100

    pt1_SizedList head

--- src/main/scala/workshop/pt1_SizedList.scala
+++ src/main/scala/workshop/pt1_SizedList.scala
@@ -1,17 +1,14 @@
 package workshop
 
 import scala.compiletime.ops.int.*
-import scala.compiletime.ops.int.given
 
 object pt1_SizedList {
 
-  trait Proof[A]
-
-  final class SizedList[N <: Int , A] private(private val values: List[A]) {
-    def ++[M <: Int ](that: SizedList[M, A]): SizedList[(N + M) , A] =
+  final class SizedList[N <: Int & Singleton, A] private(private val values: List[A]) {
+    def ++[M <: Int  & Singleton](that: SizedList[M, A]): SizedList[(N + M) & Singleton, A] =
       new SizedList(values ++ that.values)
 
-    def head(using Proof[N > 0]): A = values.head
+    def head(using (N > 0) =:= true): A = values.head
   }
 
   object SizedList {
@@ -21,13 +18,11 @@ object pt1_SizedList {
     def apply[A](a1: A, a2: A): SizedList[2, A] = new SizedList(List(a1, a2))
   }
 
-  given Proof[true] with {}
-
   val v0: SizedList[0, Int] = SizedList.empty
   val v1: SizedList[1, Int] = SizedList(1)
   val v2: SizedList[2, Int] = SizedList(2, 3)
   val v3: SizedList[3, Int] = v1 ++ v2
   val v4: SizedList[4, Int] = v1 ++ v3
-  val hV4 = v4.head
+  val hV4 = v1.head
 
 }
