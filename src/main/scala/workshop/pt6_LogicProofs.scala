b78969f5a422831991eae0fcd41e9ce2daf25064
mtu <michal.tumilowicz01@gmail.com>
2024-03-10 23:22:34 +0100

    pt6_LogicProofs

--- src/main/scala/workshop/pt6_LogicProofs.scala
+++ src/main/scala/workshop/pt6_LogicProofs.scala
@@ -2,7 +2,7 @@ package workshop
 
 import scala.language.postfixOps
 
-object pt6_LogicProofs extends App {
+object pt6_LogicProofs {
 
   type Not[P] = P => Nothing
   type Or[P, Q] = Either[P, Q]
@@ -17,19 +17,19 @@ object pt6_LogicProofs extends App {
 
   object first {
     // Some M are not P
-    trait Major:
+    trait Assumption1:
       val x: Entity;
       val value: (x.M, Not[x.P])
 
     // All M are S
-    type Minor = (x: Entity) => x.M => x.S
+    type Assumption2 = (x: Entity) => x.M => x.S
 
     // Some S are not P
     trait Thesis:
       val x: Entity;
       val value: (x.S, Not[x.P])
 
-    def proof(assumption: And[Minor, Major]): Thesis = new Thesis:
+    def proof(assumption: And[Assumption2, Assumption1]): Thesis = new Thesis:
       val major = assumption._2
       val minor = assumption._1
       val x: major.x.type = major.x
@@ -38,15 +38,15 @@ object pt6_LogicProofs extends App {
 
   object second {
     //All M are P
-    type Major = (x: Entity) => x.M => x.P
+    type Assumption1 = (x: Entity) => x.M => x.P
 
     //All S are M
-    type Minor = (x: Entity) => x.S => x.M
+    type Assumption2 = (x: Entity) => x.S => x.M
 
     //All S are P
     type Conclusion = (x: Entity) => x.S => x.P
 
-    def proof(major: Major, minor: Minor): Conclusion =
+    def proof(major: Assumption1, minor: Assumption2): Conclusion =
       x => major(x) compose minor(x)
   }
 
